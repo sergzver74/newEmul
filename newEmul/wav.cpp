bb1be5ffa903fae574e9b9a93253e71e286175b7
Зверков Сергей Владимирович <zverkov_s_v@goznak.ru>
2024-02-21 18:06:01 +0500

    add

--- newEmul/wav.cpp
+++ newEmul/wav.cpp
@@ -3,9 +3,9 @@
 static uint8_t* audio_pos;
 static uint32_t audio_len;
 
-static Uint32 wav_length; // length of our sample
-static Uint8* wav_buffer; // buffer containing our audio file
-static SDL_AudioSpec wav_spec; // the specs of our piece of music
+static Uint32 wav_length;
+static Uint8* wav_buffer;
+static SDL_AudioSpec wav_spec;
 
 
 void my_audio_callback(void* userdata, Uint8* stream, int len) {
@@ -17,8 +17,7 @@ void my_audio_callback(void* userdata, Uint8* stream, int len) {
 	}
 
 	len = (len > audio_len ? audio_len : len);
-	//SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
-	SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
+	SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);
 
 	audio_pos += len;
 	audio_len -= len;
@@ -98,25 +97,21 @@ void WAV::loadDataFromWave(std::string name) {
 						printf("bitsPerSample=%d\n", bitsPerSample);
 						clockPerSample = clock / sampleRate;
 
-						/* Load the WAV */
-						// the specs, length and buffer of our wav are filled
 						if (SDL_LoadWAV(name.c_str(), &wav_spec, &wav_buffer, &wav_length) == NULL) {
 							printf("SDL: Error loading wav\n");
 						}
-						// set the callback function
+
 						wav_spec.callback = my_audio_callback;
 						wav_spec.userdata = NULL;
-						// set our global static variables
-						audio_pos = wav_buffer; // copy sound buffer
-						audio_len = wav_length; // copy file length
 
-						/* Open the audio device */
+						audio_pos = wav_buffer;
+						audio_len = wav_length;
+
 						if (SDL_OpenAudio(&wav_spec, NULL) < 0) {
 							fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
 							exit(-1);
 						}
 
-						/* Start playing */
 						SDL_PauseAudio(0);
 
 
